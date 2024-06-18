6f2652222feef49ca1c92078d29e4a8d3278ecdd
Jens Diemer <git@jensdiemer.de>
2024-06-18 20:00:30 +0200

    Update to v0.4.1

--- django_fmd_ynh/__init__.py
+++ django_fmd_ynh/__init__.py
@@ -3,5 +3,5 @@
     YunoHost app package for https://gitlab.com/jedie/django-find-my-device
 """
 
-__version__ = '0.4.0+ynh1'
+__version__ = '0.4.1+ynh1'
 __author__ = 'Jens Diemer <git@jensdiemer.de>'
