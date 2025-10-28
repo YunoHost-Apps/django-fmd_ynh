29dc051340bc5404c59f1f9058fbac3a47fd6ea8
Jens Diemer <git@jensdiemer.de>
2025-10-28 19:53:24 +0100

    Don't support "path" installation!

--- conf/urls.py
+++ conf/urls.py
@@ -1,24 +1,15 @@
 import findmydevice
 from django.conf import settings
-from django.urls import include, path
-from django.views.generic import RedirectView
+from django.urls import path
 from django.views.static import serve
 
 
-if settings.PATH_URL:
-    # settings.PATH_URL is __PATH__
-    # Prefix all urls with "PATH_URL":
-    urlpatterns = [
-        path('', RedirectView.as_view(url=f'{settings.PATH_URL}/')),
-        path(f'{settings.PATH_URL}/', include('findmydevice_project.urls')),
-        #
-        # TODO: Serve from nginx server ;)
-        path(f'{settings.PATH_URL}/<path:path>', serve, {'document_root': findmydevice.WEB_PATH}),
-    ]
-else:
-    # Installed to domain root, without a path prefix
-    # Just use the default project urls.py
-    from findmydevice_project.urls import urlpatterns  # noqa
+assert not settings.PATH_URL, f'App must installed on domain root! Not here: {settings.PATH_URL=}'
 
-    # TODO: Serve from nginx server ;)
-    urlpatterns.append(path('<path:path>', serve, {'document_root': findmydevice.WEB_PATH}))
+
+# Installed to domain root, without a path prefix
+# Just use the default project urls.py
+from findmydevice_project.urls import urlpatterns  # noqa
+
+# TODO: Serve from nginx server ;)
+urlpatterns.append(path('<path:path>', serve, {'document_root': findmydevice.WEB_PATH}))
